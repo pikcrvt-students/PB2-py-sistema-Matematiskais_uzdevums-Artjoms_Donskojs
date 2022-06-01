806df31e1a44648d5235fc1c979083314fd75da0
Artjoms Donskojs <100723265+wayarma@users.noreply.github.com>
2022-06-01 17:04:42 +0300

    Create sistema0.py

---
+++ sistema0.py
@@ -0,0 +1,7 @@
+'''
+Autors: Artjoms Donskojs DP1-2
+'''
+
+print("Matematiskais uzdevums")
+print("Es vēlos pārbaudīt grupas DP1-2 zināšānas par sadalīšānu reizinātājos tēmu")
+print("Tas ir 0. versija")
